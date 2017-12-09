8c58c6fe926e101c7accd1e4dfd5a8f77fe21403
weresquirrel <vermokuss@gmail.com>
2017-12-09 22:14:47 +0000

    basic structure and images

--- js/script.js
+++ js/script.js
@@ -1,1 +1,48 @@
-alert('panda');
+$(() => {
+/////////
+  //PROTOTYPE
+  function Sentence(sentenceStart, sentenceFinish, rightAnswer, worth){
+    this.sentenceStart = sentenceStart;
+    this.sentenceFinish = sentenceFinish;
+    this.rightAnswer = rightAnswer;
+    this.options = [rightAnswer, 'a flamingo', 'an idiot', 'a spaniel'];
+    this.worth = worth;
+    this.incomplete = function() {
+      return `${this.sentenceStart} _______ ${this.sentenceFinish}`;
+    };
+  }
+
+  //SENENCE 1
+  const sentence1 = new Sentence('"My kingdom for', '!"', 'a horse', 1);
+  console.log(sentence1.incomplete());
+  // const suzanne = new Monkey('Susanne', 'baboon', '2015-03-25', 'banana');
+
+  //DISPLAY
+  const $h2 = $('h2');
+  const $options = $('#options');
+
+  function sentenceDisplay(incomplete) {
+    $h2.text(incomplete);
+  }
+
+  function optionsDisplay(array) {
+    for(let i = 0; i < array.length; i++) {
+      const listElement = document.createElement('li');
+      listElement.innerHTML = array[i];
+      $options.append(listElement);
+    }
+  }
+
+  sentenceDisplay(sentence1.incomplete());
+  optionsDisplay(sentence1.options);
+
+  // const $form = $('#subscribe');
+  // const $submit = $('.submit');
+  //
+  // $form.on('submit', (e) => {
+  //   e.preventDefault();
+  //   <<<<<<< HEAD
+  //   $submit.text('Submitted!');
+
+///////////
+});
